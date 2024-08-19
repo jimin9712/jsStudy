e8bfec0bce0c139c3df63561997e0dd1b665aeed
jimin9712 <ljm21000@gmail.com>
2024-08-19 12:34:28 +0900

    js init

--- i_async/d_async_await.js
+++ i_async/d_async_await.js
@@ -1,4 +1,4 @@
-// 콜백으로 계속 연결해야하기 때문에 굉장히 불편하다
+// 콜백으로 계속 연결해야하기 때문에 굉장히 불편하다.
 // const userService = (() => {
 //     const getUserList = (callback) => {
 //         fetch("https://jsonplaceholder.typicode.com/users")
@@ -16,14 +16,31 @@
 // userService.getUserList(printUsers);
 
 // async, await 문법으로 해결할 수 있다.
+// async function getName() {
+//     return "hds";
+// }
 
-async function getName() {
-    return "ljm";
-}
+// async function printName() {
+//     let result = await getName();
+//     console.log(result);
+// }
 
-async function printName() {
-    let result = await getName();
-    console.log(result);
-}
+// printName();
 
-printName();
+const userService = (() => {
+    const getUserList = async (callback) => {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+        const users = await response.json();
+        callback(users);
+    };
+
+    return { getUserList: getUserList };
+})();
+
+const printUsers = (users) => {
+    console.log(users);
+};
+
+userService.getUserList(printUsers);
---
+++ i_async/d_async_await_task.js
@@ -0,0 +1,76 @@
+// 모듈화, async, await, callback
+// jsonplaceholder에서 posts 정보를 전체 가져온 뒤
+// userId가 4인 게시글 정보들만 출력하기
+// const postService = (() => {
+//     const getPostsList = async (callback) => {
+//         const response = await fetch(
+//             "https://jsonplaceholder.typicode.com/posts"
+//         );
+//         const posts = await response.json();
+//         callback(posts);
+//     };
+
+//     return { getPostsList: getPostsList };
+// })();
+
+// const printPostsByUserId = (posts, userId) => {
+//     posts.forEach((post) => {
+//         if (post.userId === userId) {
+//             console.log(`Post ID: ${post.id}, Title: ${post.title}`);
+//         }
+//     });
+// };
+
+// postService.getPostsList((posts) => printPostsByUserId(posts, 4));
+
+// async가 함수 앞에 붙여져있으면, 리턴은 무조건 Promise객체로 감싸진다.
+// const postService = (() => {
+//     const findPosts = async (userId, callback) => {
+//         const response = await fetch(
+//             "https://jsonplaceholder.typicode.com/posts"
+//         );
+//         const posts = await response.json();
+//         return callback(userId, posts);
+//     };
+
+//     return { findPosts, findPosts };
+// })();
+
+// const getMyPosts = (userId, posts) => {
+//     return posts.filter((post) => post.userId === userId);
+// };
+
+// const printMyPosts = async () => {
+//     const myPosts = await postService.findPosts(4, getMyPosts);
+//     console.log(myPosts);
+// };
+
+// printMyPosts();
+
+// albums 데이터를 요청한 뒤, userId가 5인 정보 모두 가져오기
+//  그 중 userId와 title 출력하기
+
+const albumService = (() => {
+    const findAlbums = async (userId, callback) => {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/albums"
+        );
+        const albums = await response.json();
+        return callback(userId, albums);
+    };
+
+    return { findAlbums: findAlbums };
+})();
+
+const getMyAlbums = (userId, albums) => {
+    return albums.filter((album) => album.userId === userId);
+};
+
+const printMyAlbums = async () => {
+    const myAlbums = await albumService.findAlbums(5, getMyAlbums);
+    myAlbums.forEach((album) => {
+        console.log(`userId: ${album.userId}, title: ${album.title}`);
+    });
+};
+
+printMyAlbums();
