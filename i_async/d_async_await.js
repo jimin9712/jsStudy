efa056c022d8063949253948cd8a81cbb4103f64
jimin9712 <ljm21000@gmail.com>
2024-08-19 11:36:01 +0900

    "js init"

---
+++ i_async/d_async_await.js
@@ -0,0 +1,29 @@
+// 콜백으로 계속 연결해야하기 때문에 굉장히 불편하다
+// const userService = (() => {
+//     const getUserList = (callback) => {
+//         fetch("https://jsonplaceholder.typicode.com/users")
+//             .then((response) => response.json())
+//             .then(callback);
+//     };
+
+//     return { getUserList: getUserList };
+// })();
+
+// const printUsers = (users) => {
+//     console.log(users);
+// };
+
+// userService.getUserList(printUsers);
+
+// async, await 문법으로 해결할 수 있다.
+
+async function getName() {
+    return "ljm";
+}
+
+async function printName() {
+    let result = await getName();
+    console.log(result);
+}
+
+printName();
